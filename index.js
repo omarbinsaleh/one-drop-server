fa12cc4334decc0ec063c4b7f48f0beb6c51bb1a
omarbinsaleh <omarbinsaleh44@gmail.com>
2025-02-09 23:33:25 +0400

    added some comments in the 'validateExistinguser' middleware

--- index.js
+++ index.js
@@ -34,16 +34,27 @@ const donationRequestCollection = db.collection("donation_requests");
 
 // CUSTOM MIDDLEWARE DEFINATION: VALIDATE EXISTING USER USING EMAIL
 const validateExistingUsre = async (req, res, next) => {
+   // 01. get the user email from the request body
    const { email } = req.body;
+
+   // 02. create a filter using the user email
    const filter = { email: email }
+
+   // 03. find the specific user using the filter created above
    const isExistingUser = await userCollection.findOne(filter);
+
+   // 04. modify the request body
    if (isExistingUser) {
+      // add a property named 'isExistingUser' in the request body
       req.body.isExistingUser = true;
+      
+      // add another property 'user' in the request body which will hold the user information
       req.body.user = isExistingUser;
    } else {
       req.body.isExistingUser = false;
    }
 
+   // 05. go to the next step
    next();
 }
 
